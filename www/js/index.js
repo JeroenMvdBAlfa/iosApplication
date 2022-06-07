a15abcd243b85907170a91c976334b886d919c40
jcmvdb <mvdb.jeroen@gmail.com>
2022-06-07 14:22:53 +0200

    jquery
    put in the code for jquery

--- www/js/index.js
+++ www/js/index.js
@@ -26,10 +26,11 @@ function onDeviceReady() {
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
-
-    document.getElementById("submit").onclick = function() {myFunction()};
-    function myFunction()   {
-        var input = document.getElementById("input").value;
-        document.getElementById("result").innerHTML = input;
-    }
 }
+
+
+$(document).ready(function () {
+    $("#submit").click(function () {
+        $('#result').text($('#input').val());
+    });
+});
